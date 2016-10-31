110e886ebd145aac0e6b485337eaee1e66eab8e4
Clacson <cookiellacson@gmail.com>
2016-10-31 16:10:06 +0000

    BookcaseTest created, type, first test passed

--- BookCase.java
+++ BookCase.java
@@ -1,12 +1,14 @@
 public class Bookcase{
   private String type;
   private Book[] shelf;
-
   public Bookcase(String type) {
     this.type = type;
-
+    this.shelf = new Book[13];
   }
 
+  public String getType(){
+    return this.type;
+  }
 
 
 
